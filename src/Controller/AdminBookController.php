e8dff08a41aff7fe09fbb3350314623383b3da3a
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-11-15 15:40:44 +0100

    add form and method to create new book

--- src/Controller/AdminBookController.php
+++ src/Controller/AdminBookController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminBookController extends AbstractController
 {
@@ -14,5 +18,37 @@ class AdminBookController extends AbstractController
         return $this->render('admin_book/index.html.twig', [
             'controller_name' => 'AdminBookController',
         ]);
+    }
+
+    #[Route('/admin/book/create', name: 'app_admin_book_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, BookRepository $repository): Response
+    {
+        if ($request->isMethod(Request::METHOD_GET)) {
+
+            // render form template
+            return $this->render('admin_book/create.html.twig');
+        } elseif ($request->isMethod(Request::METHOD_POST)) {
+
+            // get form data with verification
+            $title = $request->request->has('title') ? $request->request->get('title') : false;
+            $description = $request->request->has('description') ? $request->request->get('description') : "";
+            $genre = $request->request->has('genre') ? $request->request->get('genre') : "";
+
+            if (!$title) {
+                // send to error page title empty
+            }
+
+            // add new book to repository
+            $book = (new Book())
+                ->setTitle($title)
+                ->setDescription($description)
+                ->setGenre($genre)
+                ->setCreatedAt(new DateTime())
+                ->setUpdatedAt(new DateTime());
+
+            $repository->save($book, true);
+
+            return $this->redirectToRoute('app_admin_book');
+        }
     }
 }
