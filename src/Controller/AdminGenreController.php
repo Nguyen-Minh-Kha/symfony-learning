19b3f52806c646b1b9636ee3002c7c906417e3f9
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-11-18 09:30:19 +0100

    create genre

--- src/Controller/AdminGenreController.php
+++ src/Controller/AdminGenreController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Form\GenreType;
+use App\Repository\GenreRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminGenreController extends AbstractController
 {
-    #[Route('/admin/genre', name: 'app_admin_genre')]
-    public function index(): Response
+    // #[Route('/admin/genre', name: 'app_admin_genre')]
+    // public function index(): Response
+    // {
+    //     return $this->render('admin_genre/index.html.twig', [
+    //         'controller_name' => 'AdminGenreController',
+    //     ]);
+    // }
+
+    #[Route('/admin/genres/create', name: "app_AdminGenreController_create", methods: ['GET', 'POST'])]
+    public function create(Request $request, GenreRepository $genreRepository): Response
     {
-        return $this->render('admin_genre/index.html.twig', [
-            'controller_name' => 'AdminGenreController',
+        $form = $this->createForm(GenreType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $genre = $form->getData();
+
+            $genre
+                ->setCreatedAt(new DateTime())
+                ->setUpdatedAt(new DateTime());
+
+            $genreRepository->save($genre, true);
+
+            return $this->redirectToRoute('');
+        }
+        return $this->render('admin_genre/create.html.twig', [
+            'genreForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/GenreType.php
+++ src/Form/GenreType.php
@@ -6,6 +6,7 @@ use App\Entity\Genre;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class GenreType extends AbstractType
 {
@@ -13,16 +14,27 @@ class GenreType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('description')
-            ->add('createdAt')
-            ->add('updatedAt')
-        ;
+            ->add('description');
+        if ($options['mode'] === 'create') {
+            $builder->add(
+                'submit',
+                SubmitType::class,
+                [
+                    'label' => 'Add new Genre',
+                ]
+            );
+        } else {
+            $builder->add('submit', SubmitType::class, [
+                'label' => 'Update Genre',
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Genre::class,
+            'mode' => 'create',
         ]);
     }
 }
