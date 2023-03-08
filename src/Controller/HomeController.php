c72eeb65aa68a52cd3721ad47a5fcc22832bc5cc
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2023-03-08 16:53:03 +0100

    sign up, sign in, security overhaul

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(BookRepository $bookRepository): Response
     {
-        return new Response("Page d'accueil");
+
+        $books = $bookRepository->findAll();
+
+        return $this->render('home/index.html.twig',[
+            'books' => $books
+        ]);
     }
+
+    
 }
