a610e6202165db70ce97696ff24d00de64f0833d
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2023-03-08 16:30:14 +0100

    remove unused

--- src/Controller/AdminBookController.php
+++ src/Controller/AdminBookController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdminBookController extends AbstractController
 {
