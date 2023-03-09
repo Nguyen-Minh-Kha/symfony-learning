cfdb7021cc7a79874e861014d743e61e833e64a6
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2023-03-09 15:10:46 +0100

    paginate styling

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,20 +3,28 @@
 namespace App\Controller;
 
 use App\Repository\BookRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home_index', methods: ['GET'])]
-    public function index(BookRepository $bookRepository): Response
+    public function index(Request $request, BookRepository $bookRepository, PaginatorInterface $paginator): Response
     {
 
         $books = $bookRepository->findAll();
 
+        $data = $paginator->paginate(
+            $books,
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('home/index.html.twig',[
-            'books' => $books
+            'books' => $data
         ]);
     }
 
